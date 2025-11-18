6e2c0865acb6ea40b2ea315da8447f43f0d44302
Tanay Grover <grover.t@northeastern.edu>
2025-11-17 20:23:08 -0500

    Added fronted deployment

--- tatertot-frontend/vite.config.js
+++ tatertot-frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),  // Add Tailwind plugin
+    tailwindcss(),
   ],
+  base: '/TaterTot/',  // Replace with your actual repo name
 })
